875cc76cb60dec8d2bb94c38a06a034d3355388f
Theo <theocaldas28@gmail.com>
2021-01-26 15:37:44 -0300

    Initial Commit

---
+++ Face Rhythm/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Face Rhythm
+//
+//  Created by Theo Necyk Agner Caldas on 26/01/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
---
+++ Face Rhythm/MidiParser Source/MidiEvent/Meta/MidiTime.swift
@@ -0,0 +1,28 @@
+//
+//  struct.swift
+//  MidiParser iOS
+//
+//  Created by Vladimir Vybornov on 7/1/19.
+//  Copyright © 2019 Yuma Matsune. All rights reserved.
+//
+
+import Foundation
+
+public struct MidiTime {
+    let inSeconds: TimeInterval
+    let inTicks: Ticks
+}
+
+public extension MidiTime {
+    static var empty: MidiTime {
+        return MidiTime(inSeconds: 0.0, inTicks: Ticks(0))
+    }
+}
+
+///Minhas cagadas
+public extension MidiTime{
+    func getTimeInSeconds() -> TimeInterval{
+        return self.inSeconds
+    }
+}
+
