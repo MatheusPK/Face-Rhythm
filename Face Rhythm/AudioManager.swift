20a7b5ad39e6169039f6d38f6e63fbfbd6c6a691
Theo <theocaldas28@gmail.com>
2021-01-26 15:59:54 -0300

    Adding Audio Manager and Midi Manager

---
+++ Face Rhythm/AudioManager.swift
@@ -0,0 +1,90 @@
+//
+//  AudioManager.swift
+//  midiTest_21-01
+//
+//  Created by Theo Necyk Agner Caldas on 25/01/21.
+//
+
+import Foundation
+import AVFoundation
+
+class AudioManager{
+    
+    private let engine = AVAudioEngine()
+    private let speedControl = AVAudioUnitVarispeed()
+    private let pitchControl = AVAudioUnitTimePitch()
+    private let audioPlayerNode = AVAudioPlayerNode()
+    
+    private var singleNoteFile = AVAudioFile()
+    
+    private var musicAudioPlayer = AVAudioPlayer()
+    
+    static let singleInstance = AudioManager()
+    
+    private init(){
+        do{
+            try self.loadEngine()
+        }catch{
+            print("Erro ao carregar Engine!", #function)
+        }
+        self.loadPlayer()
+    }
+    
+    private func loadEngine() throws{
+        if let note = Bundle.main.url(forResource: "C3", withExtension: ".mp3"){
+            self.singleNoteFile = try AVAudioFile(forReading: note)
+        }
+        else{
+            print("Arquivo não encontrado!", #function)
+        }
+            
+        engine.attach(audioPlayerNode)
+        engine.attach(pitchControl)
+        engine.attach(speedControl)
+        
+        engine.connect(audioPlayerNode, to: speedControl, format: nil)
+        engine.connect(speedControl, to: pitchControl, format: nil)
+        engine.connect(pitchControl, to: engine.mainMixerNode, format: nil)
+        
+        audioPlayerNode.volume = 3
+        try engine.start()
+    }
+    
+    private func loadPlayer(){
+        do{
+            if let mp3File = Bundle.main.url(forResource: "C5.5 - 1.1 full", withExtension: ".mp3"){
+                self.musicAudioPlayer = try AVAudioPlayer(contentsOf: mp3File)
+            }else{
+                print("Arquivo mp3 nao encontrado!")
+                return
+            }
+        }catch{
+            print("Impossivel carregar AVAudioPlayer!")
+            return
+        }
+    }
+    
+    public func playNote(note: UInt8){
+        let midiNumberC3 = 48
+        self.pitchControl.pitch = Float(100*(Int(note) - midiNumberC3))
+        self.audioPlayerNode.scheduleFile(singleNoteFile, at: nil, completionHandler: nil)
+        self.audioPlayerNode.play()
+    }
+    
+    public func stopNote(){
+        self.audioPlayerNode.stop()
+    }
+    
+    public func playMusic(){
+        self.musicAudioPlayer.prepareToPlay()
+        self.musicAudioPlayer.play()
+    }
+    
+    public func getMusicTimeStamp() -> TimeInterval{
+        return self.musicAudioPlayer.currentTime
+    }
+    
+    
+    
+    
+}
---
+++ Face Rhythm/MidiManager.swift
@@ -0,0 +1,66 @@
+//
+//  MidiManager.swift
+//  midiTest_21-01
+//
+//  Created by Theo Necyk Agner Caldas on 25/01/21.
+//
+
+import Foundation
+import AVFoundation
+import MidiParser
+
+protocol MidiManagerDelegate{
+    func noteOn(note: UInt8)
+    func noteOff()
+}
+
+class MidiManager{
+    
+    private let midiData = MidiData()
+    private var delegates = [MidiManagerDelegate]()
+    
+    static let singleInstance = MidiManager()
+    
+    private init(){
+        self.loadFile()
+    }
+    
+    public func addDelegate(delegate: MidiManagerDelegate){
+        self.delegates.append(delegate)
+    }
+    
+    private func loadFile(){
+        guard let midiFile = Bundle.main.url(forResource: "C5.5 - 1.1", withExtension: ".mid") else{
+            print("Impossivel carregar arquivo Midi!")
+            return
+        }
+        
+        guard let data = try? Data(contentsOf: midiFile) else {
+            print("Impossivel converter pro tipo Data!")
+            return
+        }
+        
+        self.midiData.load(data: data)
+    }
+    
+    public func checkNoteOn() {
+        let track = self.midiData.noteTracks[0]
+
+        for note in track{
+            if note.timeStamp.getTimeInSeconds().roundTo(places: 2) == AudioManager.singleInstance.getMusicTimeStamp().roundTo(places: 2){
+                
+                for delegate in self.delegates{
+                    delegate.noteOn(note: note.note)
+                }
+                
+                let _ = Timer.scheduledTimer(timeInterval: note.duration.getTimeInSeconds(), target: self, selector: #selector(scheduleNoteOff), userInfo: nil, repeats: false)
+            }
+        }
+    }
+    
+    @objc private func scheduleNoteOff(){
+        for delegate in self.delegates{
+            delegate.noteOff()
+        }
+    }
+}
